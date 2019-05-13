7a99a1dcaf3b8ec8385ebd37642080d80084c0f9
Malo Misson <MaloMisson@MacBook-Pro-de-Malo.local>
2019-05-13 12:06:04 +0200

    modif travis test

--- tests/testTravis.js
+++ tests/testTravis.js
@@ -1,1 +1,14 @@
-return true;
+var assert = require('assert'),
+    vows = require('vows');
+
+vows.describe('test1').addBatch({
+    'premier test':{
+        topic : function(){
+            return 1;
+        },
+        'resultat':function(topic){
+            assert.isNumber(topic);
+            assert.equal(topic,1);
+        }
+    }
+}).export(module);
