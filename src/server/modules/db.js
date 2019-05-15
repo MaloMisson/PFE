c0b3e0055cfc32b78eaf40fac235eea25a87474f
Jeremy <jeremy@MacBook-Pro-de-Malo.local>
2019-05-15 13:44:09 +0200

    Ajout .env info connection db

--- src/server/modules/db.js
+++ src/server/modules/db.js
@@ -1,5 +1,11 @@
 const { Client } = require('pg');
 
+/*const dbUser = prosess.env.DB_USER;
+const dbHost = prosess.env.DB_HOST;
+const dbDatabase = prosess.env.DB_DATABASE;
+const dbPassword = prosess.env.DB_PASSWORD;
+const dbPort = prosess.env.DB_PORT;*/
+
 let db = (function(){
     const db = new Client({
         user: 'hzewnisw',
@@ -9,49 +15,53 @@ let db = (function(){
         port: 5432,
     });
 
-    db.connect();
-
     // SELECT USER BY PSEUDO
     function selectUser(user){
         const queryText = 'SELECT * FROM pfe.users WHERE pseudo = $1';
         const values = [user.pseudo];
         let result;
+        db.connect();
         db.query(queryText, values, (err, res) => {
             if (err) {
                 result = null;
             } else {
                 result = res.rows[0];
             }
+            db.end();
             return result;
         });
     }
 
-    // INSERT UTILISATEUR
+    // INSERT USER
     function insertUser(user){
         const queryText = 'INSERT INTO pfe.users (pseudo,firstname,lastname,password,address,number,zip_code,city,country,email,phone) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *';
         const values = [user.pseudo,user.firstname,user.lastname,user.password,user.address,user.number,user.zip_code,user.city,user.country,user.email,user.phone];
         let result;
+        db.connect();
         db.query(queryText, values, (err, res) => {
             if (err) {
                 result = null;
             } else {
                 result = res.rows[0];
             }
+            db.end();
             return result;
         })
     }
 
-    // UPDATE UTILISATEUR
+    // UPDATE USER
     function updateUser(user){
         const queryText = 'UPDATE pfe.users SET pseudo = $1, firstname = $2, lastname = $3, password = $4, address = $5, number = $6, zip_code = $7, city = $8, country = $9, email = $10, phone = $11 WHERE id_user = $12 RETURNING *';
         const values = [user.pseudo,user.firstname,user.lastname,user.password,user.address,user.number,user.zip_code,user.city,user.country,user.email,user.phone,user.id_user];
         let result;
+        db.connect();
         db.query(queryText, values, (err, res) => {
             if (err) {
                 result = null;
             } else {
                 result = res.rows[0];
             }
+            db.end();
             return result;
         })
     }
@@ -60,12 +70,14 @@ let db = (function(){
     function selectProducts(){
         const queryText = 'SELECT * FROM pfe.products';
         let result;
+        db.connect();
         db.query(queryText, values, (err, res) => {
             if (err) {
                 result = null;
             } else {
                 result = res.rows;
             }
+            db.end();
             return result;
         });
     }
@@ -75,12 +87,14 @@ let db = (function(){
         const queryText = 'INSERT INTO pfe.products (id_seller,id_buyer,state,description,price) VALUES ($1,$2,$3,$4,$5) RETURNING *';
         const values = [product.id_seller,product.id_buyer,product.state,product.description,product.price];
         let result;
+        db.connect();
         db.query(queryText, values, (err, res) => {
             if (err) {
                 result = null;
             } else {
                 result = res.rows[0];
             }
+            db.end();
             return result;
         })
     }
@@ -90,18 +104,16 @@ let db = (function(){
         const queryText = 'UPDATE pfe.products SET id_seller = $1, id_buyer = $2, state = $3, description = $4, price = $5 WHERE id_product = $6 RETURNING *';
         const values = [aticle.id_seller,product.id_buyer,product.state,product.description,product.price,product.id_product];
         let result;
+        db.connect();
         db.query(queryText, values, (err, res) => {
             if (err) {
                 result = null;
             } else {
                 result = res.rows[0];
             }
+            db.end();
             return result;
         })
-    }
-
-    function close(){
-        db.end();
     }
 
     var self = {
@@ -110,7 +122,7 @@ let db = (function(){
         updateUser : updateUser,
         selectProducts : selectProducts,
         insertProduct : insertProduct,
-        updateProduct : updateProduct
+        updateProduct : updateProduct,
     };
     return self;
 })();
