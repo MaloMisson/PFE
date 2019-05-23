0033b974c55ee7ccd9137a06f7f0abd36c6a0777
Arturo Mozzon <arturo@mozzon.net>
2019-05-23 02:57:36 +0200

    Add cors for frontend

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require('./modules/db1.js');
 const path = require('path');
 var session = require('express-session')
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
